a126c5031b6b78922617e0a156376e7f67f1034a
applul <ray.houston44@gmail.com>
2019-06-11 07:17:49 +0800

    copypasted from my desktop

---
+++ script.js
@@ -0,0 +1,24 @@
+const fs = require('fs'); 
+
+fs.readFile('./Hello.txt', (err, data) => {
+    if (err) {
+        console.log('Errror! Also, Holo I love you!');
+    }
+    console.log("Async", data.toString('utf8'));
+})
+
+const file = fs.readFileSync('./Hello.txt');
+
+console.log("Sync", file.toString());
+
+fs.appendFile('./Hello.txt', ' Hello, Holo', err => {
+    if (err) {
+        console.log(1, err)
+    }
+});
+
+fs.appendFile('./Seiba', 'Hello, Seiba', err => {
+    if (err) {
+        console.log(2, err)
+    }
+});
